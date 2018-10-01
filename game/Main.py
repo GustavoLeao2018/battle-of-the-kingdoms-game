800c791d77c6c4e1737058004684db91d2144fef
GustavoLeao2018 <gustavo.leao.nogueira.2018@gmail.com>
2018-10-01 00:08:42 -0300

    Alterado o jogo.

--- game/Main.py
+++ game/Main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Jogo criado com python3, chamado  de  Battle of the Kingdoms."""
+from time import sleep
 
 from pygame import *
 from sys import exit
@@ -17,6 +18,16 @@ def arvore(janela, x, y):
 def arqueiro(janela, x, y):
     arqueiro_imagem = image.load("img/arqueiro.png")
     janela.blit(arqueiro_imagem, (x, y))
+    caminho = arqueiro_imagem.get_rect()
+    return arqueiro_imagem, caminho
+
+def flecha(janela, x, y):
+    flecha_imagem = image.load("img/flecha.png")
+    janela.blit(flecha_imagem, (x, y))
+    caminho = flecha_imagem.get_rect()
+    return flecha_imagem, caminho
+
+
 
 def tela():
     init()
@@ -35,18 +46,43 @@ def tela():
     x_arqueiro = 100
     y_arqueiro = 100
 
+    x_flecha = -100
+    y_flecha = -100
+
+    alt = 0
+    larg = 0
+
+    mouse.set_cursor(*cursors.broken_x)
+
+    contador_vezes = 0
     while True:
         for evento in event.get():
             if evento.type == QUIT:
                 exit()
-            if evento.type == KEYDOWN and evento.key == K_w:
-                y_arqueiro -= 2
-            if evento.type == KEYDOWN and evento.key == K_s:
-                y_arqueiro += 2
-            if evento.type == KEYDOWN and evento.key == K_a:
-                x_arqueiro -= 2
-            if evento.type == KEYDOWN and evento.key == K_d:
-                x_arqueiro += 2
+            if evento.type == KEYDOWN:
+                if evento.key == K_w:
+                    y_arqueiro -= 2
+                    contador_vezes = 0
+                if evento.key == K_s:
+                    y_arqueiro += 2
+                    contador_vezes = 0
+                if evento.key == K_a:
+                    x_arqueiro -= 2
+                    contador_vezes = 0
+                if evento.key == K_d:
+                    x_arqueiro += 2
+                    contador_vezes = 0
+                if evento.key == K_KP7:
+                    if contador_vezes == 0:
+                        x_flecha = x_arqueiro
+                        y_flecha = y_arqueiro
+                        contador_vezes = 1
+                    else:
+                        for cont in range(5):
+                            x_flecha += 5
+                            flecha_img, caminho = flecha(janela, x_flecha, y_flecha)
+
+
         keys_pressed = key.get_pressed()
         if keys_pressed[K_w]:
             y_arqueiro -= 2
@@ -54,6 +90,7 @@ def tela():
             y_arqueiro += 2
         if keys_pressed[K_a]:
             x_arqueiro -= 2
+
         if keys_pressed[K_d]:
             x_arqueiro += 2
 
@@ -62,7 +99,12 @@ def tela():
         for cont in range(quantidade):
             arvore(janela, x[cont], y[cont])
 
-        arqueiro(janela, x_arqueiro, y_arqueiro)
+        arqueiro_img, caminho_arqueiro = arqueiro(janela, x_arqueiro, y_arqueiro)
+
+
+        flecha_img, caminho = flecha(janela, x_flecha, y_flecha)
+
+
 
         tempo.tick(60)
 
